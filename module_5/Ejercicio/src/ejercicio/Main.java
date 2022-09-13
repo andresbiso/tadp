d77b9da10bb64a1b2267e73e583e0de18a5cf343
Andrés Biso <biso.andres@gmail.com>
2022-09-13 14:16:04 -0300

    Update Main.java

--- module_5/Ejercicio/src/ejercicio/Main.java
+++ module_5/Ejercicio/src/ejercicio/Main.java
@@ -7,19 +7,17 @@ public class Main {
 
 	public static void main(String[] args) {
 	    List<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
-	    ingredientes.add(new Ingrediente("caja de brownies"));
-	    ingredientes.add(new Ingrediente("2 huevos"));
-	    ingredientes.add(new Ingrediente("queso crema"));
-	    ingredientes.add(new Ingrediente("manteca"));
-	    ingredientes.add(new Ingrediente("harina"));
+	    ingredientes.add(new Ingrediente("Tomate",2,"kg"));
+	    ingredientes.add(new Ingrediente("Lechuga",1,"kg"));
+	    ingredientes.add(new Ingrediente("Zanahoria",2,"kg"));
 
 	    List<Instruccion> instrucciones = new ArrayList<Instruccion>();
-	    instrucciones.add(new Instruccion(1, "Enmantecar y harinar molde"));
-	    instrucciones.add(new Instruccion(2, "Mezclar huevos, manteca y brownies hasta lograr consistencia homogenea"));
-	    instrucciones.add(new Instruccion(3, "Volcar preparacion en el molde y colocar cucharadas de queso crema por toda la superficie"));
-	    instrucciones.add(new Instruccion(4, "Llevar al horno por 25 minutos"));
+	    instrucciones.add(new Instruccion(1, "Lavar y cortar el tomate"));
+	    instrucciones.add(new Instruccion(2, "Lavar y cortar la lechuga"));
+	    instrucciones.add(new Instruccion(3, "Lavar, pelar y cortar la zanahoria"));
+	    instrucciones.add(new Instruccion(4, "Colocar todo en un recipiente, condimentar con aceite y mezclar"));
 
-	    Receta miReceta = new Receta("torta brownie", ingredientes, instrucciones);
+	    Receta miReceta = new Receta("ensalada", ingredientes, instrucciones);
 	    miReceta.detallesReceta();
 	}
 
