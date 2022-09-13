2edc155508c93cd2164640969cb5601da0edbc14
Andrés Biso <biso.andres@gmail.com>
2022-09-13 14:11:44 -0300

    Update Ingrediente.java

--- module_5/Ejercicio/src/ejercicio/Ingrediente.java
+++ module_5/Ejercicio/src/ejercicio/Ingrediente.java
@@ -1,15 +1,42 @@
-package ejercicio;
+package com.tadp.spring.recipe;
 
 public class Ingrediente {
-	
-	private String descripcion;
+	private String nombre;
+	private float cantidad;
+	private String unidad;
 
-	public Ingrediente(String descripcion) {
-	  this.descripcion = descripcion;
+	public Ingrediente(String nombre, float cantidad, String unidad) {
+	  setNombre(nombre);
+	  setCantidad(cantidad);
+	  setUnidad(unidad);
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(float cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
+	}
+
 	@Override
 	public String toString(){
-	    return descripcion;
+	    return nombre + "-" + cantidad + unidad;
 	}
 }
